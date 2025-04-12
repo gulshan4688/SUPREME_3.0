2fbd8fbaeabb74bad9a6dfcceb78cdafbcceb89f
Gulshan kumar <gulshankumar4688@gmail.com>
2025-04-12 14:04:52 +0530

    till const keyword

---
+++ week_09_OOPS/Const_keyword/const.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class abc {
+    int x;
+    int *y;
+    public:
+
+    abc(){
+        x=0;
+        y=new int(0);
+    }
+    int getX(){
+        return x;
+    }
+    int setX(int val){
+        x=val;
+    }
+    int getY(){
+        return *y;
+    }
+    int setX(int val){
+        *y=val;
+    }
+    
+};
+int main(){
+
+}
+int main2()
+{
+    const int s=4;
+    cout<<s<<endl;
+    const int *p=new int(10);
+    cout<<*p<<endl;
+    int b=11;
+    // *p=&b;   cant change the content of the  pointer but below pointer itself can ve re-assign 
+    p=&b; 
+    cout<<*p<<endl;
+    return 0;
+}
